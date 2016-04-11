61e9c1ca41b009f1bbe33417c8ad2f154db8a2b9
jwalsh <jwalsh@whispir.com>
2016-04-11 17:32:26 +1000

    updated sources endpoint

--- controllers/DefaultService.js
+++ controllers/DefaultService.js
@@ -6,24 +6,8 @@ exports.sourcesGET = function(args, res, next) {
   * name (String)
   * type (String)
   **/
-  
-  
-  var examples = {};
-  examples['application/json'] = [ {
-  "license" : "aeiou",
-  "visibility" : "aeiou",
-  "name" : "aeiou",
-  "type" : "aeiou",
-  "url" : "aeiou",
-  "authentication" : {
-    "password" : "aeiou",
-    "apikey" : "aeiou",
-    "authentication" : "aeiou",
-    "username" : "aeiou"
-  }
-} ];
-  
-  if(Object.keys(examples).length > 0) {
+    var examples = {};
+    if(Object.keys(examples).length > 0) {
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
   }
@@ -31,17 +15,21 @@ exports.sourcesGET = function(args, res, next) {
     res.end();
   }
   
-  
 }
 
 exports.sourcesPOST = function(args, res, next) {
   /**
    * parameters expected in the args:
-  * source (InformationSource)
+  * source (InformationSourceRequest)
   **/
-  // no response value expected for this operation
+    var examples = {};
+    if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
   
-  
-  res.end();
 }
 
